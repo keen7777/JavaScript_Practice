77cd76895dd9f6b0978b414d5fc2f3fb9dbad8e9
Keen <can.wang.keen@gmail.com>
2025-12-08 23:35:47 +0100

    ch12, advanced function, modify rps game, adding autoplay and stop logic

---
+++ scripts/11_todo_list.js
@@ -0,0 +1,79 @@
+const todo_list = [{
+    name: 'test make dinner',
+    dueDate: '2025-11-27'
+},{
+    name: 'test do laundry',
+    dueDate: '2025-11-28'
+}
+];
+
+
+// this function is to generating the html
+function renderTodoList() {
+    let todo_list_html = '';
+    for (let i = 0; i < todo_list.length; i++) {
+        const todo = todo_list[i];
+        const html_todo = `
+        <div> ${todo.name} </div>
+        <div> ${todo.dueDate} </div>
+        <button class="delete-todo-btn" data-index="${i}" >Delete</button> 
+        `;
+        todo_list_html = todo_list_html + html_todo;
+    }
+
+    // console.log(todo_list_html);
+    document.querySelector('.js-todo-list').innerHTML = todo_list_html;
+}
+
+
+function addTodo() {
+    const input_name = document.querySelector('.js-name-input');
+    const todo_name = input_name.value;
+
+    const input_due_date = document.querySelector('.js-due-date-input');
+    const todo_due_date = input_due_date.value;
+    // empty input do not add!
+    if (todo_name != '') {
+        todo_list.push({
+            name: todo_name,
+            dueDate: todo_due_date}
+        );
+        // console.log(todo_list);
+    }
+
+
+    // clear the input field
+    input_name.value = '';
+    input_due_date.value = '';
+    renderTodoList();
+}
+
+function deleteTodo(idx) {
+    todo_list.splice(idx, 1);
+    renderTodoList();
+    // console.log(todo_list);
+}
+
+
+document.body.addEventListener('click', (e) => {
+    const button = e.target.closest('button');
+    // only for buttons.
+    if (!button) {
+        return;
+    }
+
+    const id = button.id;
+
+    if (id === 'add-todo-btn') {
+        addTodo();
+        // put render inside addTodo functions
+        // renderTodoList();
+    }
+
+    if (button.classList.contains('delete-todo-btn')) {
+        const idx = Number(button.dataset.index);
+        deleteTodo(idx);
+    }
+
+
+}); 
